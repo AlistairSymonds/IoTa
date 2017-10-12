b8c1f5d22bd729825618427aa7347faceda23524
AlistairSymonds <alistair.symonds@gmail.com>
2017-10-12 23:47:09 +1100

    rainbow animation

---
+++ embedded/LedSerialSlave_octo_teensy/LedSerialSlave_octo_teensy/RainbowAnim.cpp
@@ -0,0 +1,20 @@
+#include "RainbowAnim.h"
+#include "LedPropEnum.h"
+
+
+
+RainbowAnim::RainbowAnim(CHSV * leds, int numleds, uint8_t * state)
+{
+	animBufPtr = leds;
+	statePtr = state;
+	RainbowAnim::numleds = numleds;
+}
+
+void RainbowAnim::advanceAnim()
+{	
+	fill_rainbow(animBufPtr, numleds, statePtr[hue], 15);
+}
+
+RainbowAnim::~RainbowAnim()
+{
+}
