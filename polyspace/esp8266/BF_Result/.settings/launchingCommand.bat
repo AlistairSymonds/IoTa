d15d98ef69a6b582600e8611a806562522415716
Sayfog <alistair.symonds@gmail.com>
2018-04-27 13:44:01 +1000

    poplyspace rubbish

---
+++ polyspace/esp8266/BF_Result/.settings/launchingCommand.bat
@@ -0,0 +1,2 @@
+@echo off
+"D:\Programs\Matlab r2018a\polyspace\bin\polyspace-bug-finder-nodesktop.exe" -options-file "C:\Users\alist\OneDrive\Development\IoTa\polyspace\esp8266\BF_Result\.settings\options_command.txt" -results-dir "C:\Users\alist\OneDrive\Development\IoTa\polyspace\esp8266\BF_Result" %*
