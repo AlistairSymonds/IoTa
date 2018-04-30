fc1c97176966cfe1625f0e9df8319b3a25484b09
Sayfog <alistair.symonds@gmail.com>
2018-04-30 21:03:52 +1000

    Accessing both SPI buses on ESP32 via arduino api

--- embedded/Esp32Scope/Esp32Scope/Esp32Scope.ino
+++ embedded/Esp32Scope/Esp32Scope/Esp32Scope.ino
@@ -10,12 +10,21 @@
 int debugPin = 35;
 int debugPinVal = 1;
 
+SPIClass * vspi = NULL;
+SPIClass * hspi = NULL;
+
 hw_timer_t * sampleTimer = NULL; 
 void setup() {
 	Serial.begin(9600);
+
+	vspi = new SPIClass(3);
+	hspi = new SPIClass(2);
+	//set up slave select pins as outputs
 	pinMode(5, OUTPUT);
-	digitalWrite(5, HIGH);
-	SPI.begin(14, 12, 13, 5);
+	pinMode(15, OUTPUT);
+	//clock miso mosi ss
+	vspi->begin(); //(18, 19, 23, 5);
+	hspi->begin(); //(14, 12, 13, 15);
 
 	pinMode(debugPin, OUTPUT);
 	sampleTimer = timerBegin(0, 80, true);
@@ -27,34 +36,31 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 	//do nothing
-
+	setGain();
+	getADC();
+	delay(100);
 }
 
 void setGain() {
 	byte cmdType = 0b0010101;
 	byte cmdData = 0b00010010;
-	SPI.beginTransaction(SPISettings(10000, MSBFIRST, SPI_MODE0));
 
-	digitalWrite(5, LOW);
-	delayMicroseconds(100);
-	SPI.transfer(cmdType);
-	SPI.transfer(cmdData);
-	delayMicroseconds(100);
-	digitalWrite(5, HIGH);
+	vspi->beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+	vspi->transfer(cmdType);
+	vspi->transfer(cmdData);
+	vspi->endTransaction();
+}
 
-	SPI.endTransaction();
+void getADC() {
+	byte stuff = 0b11001100;
+	hspi->beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+	hspi->transfer(stuff);
+	hspi->endTransaction();
 }
 
 void IRAM_ATTR timerFunction() {
 	//portENTER_CRITICAL_ISR(&sampleTimer);
-	Serial.println("Interrupt!");
-	digitalWrite(debugPin, debugPinVal);
-	if (debugPinVal == 1) {
-		debugPinVal = 0;
-	}
-	else
-	{
-		debugPinVal = 1;
-	}
+	
+	
 	//portEXIT_CRITICAL_ISR(&sampleTimer);
 }
