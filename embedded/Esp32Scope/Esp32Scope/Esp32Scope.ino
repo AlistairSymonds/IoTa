d26caa852141e6e94f8cfcc68265ead4eb55700e
Sayfog <alistair.symonds@gmail.com>
2018-05-02 00:35:39 +1000

    Cleaned up esp32 spi testing
    Proper example here: https://github.com/espressif/arduino-esp32/pull/1368

--- embedded/Esp32Scope/Esp32Scope/Esp32Scope.ino
+++ embedded/Esp32Scope/Esp32Scope/Esp32Scope.ino
@@ -3,9 +3,11 @@
  Created:	4/22/2018 3:40:48 PM
  Author:	alist
 */
+
 #include <SPI.h>
 // the setup function runs once when you press reset or power the board
 
+static const int spiClk = 1000000; // 1 MHz
 
 int debugPin = 35;
 int debugPinVal = 1;
@@ -17,14 +19,16 @@ hw_timer_t * sampleTimer = NULL;
 void setup() {
 	Serial.begin(9600);
 
-	vspi = new SPIClass(3);
-	hspi = new SPIClass(2);
+	vspi = new SPIClass(VSPI);
+	hspi = new SPIClass(HSPI);
 	//set up slave select pins as outputs
 	pinMode(5, OUTPUT);
 	pinMode(15, OUTPUT);
 	//clock miso mosi ss
+
 	vspi->begin(); //(18, 19, 23, 5);
 	hspi->begin(); //(14, 12, 13, 15);
+
 
 	pinMode(debugPin, OUTPUT);
 	sampleTimer = timerBegin(0, 80, true);
@@ -44,17 +48,21 @@ void loop() {
 void setGain() {
 	byte cmdType = 0b0010101;
 	byte cmdData = 0b00010010;
-
-	vspi->beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+	
+	vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+	digitalWrite(5, LOW);
 	vspi->transfer(cmdType);
 	vspi->transfer(cmdData);
+	digitalWrite(5, HIGH);
 	vspi->endTransaction();
 }
 
 void getADC() {
 	byte stuff = 0b11001100;
-	hspi->beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+	hspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+	digitalWrite(15, LOW);
 	hspi->transfer(stuff);
+	digitalWrite(15, HIGH);
 	hspi->endTransaction();
 }
 
